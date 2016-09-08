58b39b888d1e25c08ce5184ffa25f7c9cb3dc274
theDogOfPavlov <dog@pavlov.com>
2016-09-08 22:52:04 +0100

    Update JUnitXmlTestReporter.h

--- UnitTest++/JUnitXmlTestReporter.h
+++ UnitTest++/JUnitXmlTestReporter.h
@@ -7,11 +7,17 @@ namespace UnitTest {
 
    class UNITTEST_LINKAGE JUnitXmlTestReporter : public TestReporter
    {
+   public:
+      explicit JUnitXmlTestReporter(std::ostream& ostream);
+      
    private:
+      virtual JUnitXmlTestReporter();
       virtual void ReportTestStart(TestDetails const& test);
       virtual void ReportFailure(TestDetails const& test, char const* failure);
       virtual void ReportTestFinish(TestDetails const& test, float secondsElapsed);
       virtual void ReportSummary(int totalTestCount, int failedTestCount, int failureCount, float secondsElapsed);
+      
+      std::ostream& m_ostream;
    };
 
 }
